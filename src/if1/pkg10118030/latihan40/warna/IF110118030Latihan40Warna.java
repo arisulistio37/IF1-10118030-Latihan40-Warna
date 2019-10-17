1cf0f81b46fb52969663e6d3f1ea6d6d4909d08a
arisulistio37 <arisulistio37@gmail.com>
2019-10-17 19:51:49 +0700

    Initial commit

---
+++ src/if1/pkg10118030/latihan40/warna/IF110118030Latihan40Warna.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118030.latihan40.warna;
+
+/**
+ *
+ * @author
+ * NAMA         : Ari Sulistio
+ * KELAS        : IF-1
+ * NIM          : 10118030
+ * Deskripsi program : program ini berisi tentang warna kepribadian
+ * 
+ */
+public class IF110118030Latihan40Warna {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Warna warna = new Warna();
+        warna.iWarnaMu();
+    }
+    
+}
